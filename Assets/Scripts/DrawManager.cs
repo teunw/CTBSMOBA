12024ff8fc4908ba1ae8ec9d745ac3dd307502a2
Teun <teun.willems@live.nl>
2016-05-26 14:30:30 +0200

    Filled Drawmanager

--- Assets/Scripts/DrawManager.cs
+++ Assets/Scripts/DrawManager.cs
@@ -1,19 +1,61 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class DrawManager : MonoBehaviour {
+[RequireComponent(typeof(LineRenderer))]
+public class DrawManager : MonoBehaviour
+{
+    public readonly Color DefaultColor = new Color(0f, 0f, 0f);
+
+    public float LineWidth = .5f;
+
+    public GameObject DrawPlane;
+    public Camera PlayerCamera;
+    public GameObject SelectedMember;
+
+    public LineRenderer LineRenderer
+    {
+        get; private set;
+    }
 
 	private int Current;
+    private List<Vector2> Positions;
 
-	public void GetCurrentDrawManager() {
+    void Start()
+    {
+        LineRenderer = GetComponent<LineRenderer>();
+    }
 
-	}
+    void Update()
+    {
+        if (IsMemberSelected)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector2 mousePosition = Input.mousePosition;
+
+                Positions.Add(mousePosition);
+                LineRenderer.SetPosition(Current, mousePosition);
+
+                Current++;
+            }
+            else
+            {
+                SelectedMember = null;
+                SetAction();
+            }
+        }
+    }
+
+    public bool IsMemberSelected
+    {
+        get
+        {
+            return SelectedMember != null;
+        }
+    }
 
 	public void SetAction() {
-
+        WalkAction walkAction = new WalkAction(this.Positions);
 	}
-
-	public void Draw() {
-
-	}
-
 }
