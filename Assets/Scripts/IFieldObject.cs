e40871724c967a39ed38a784cd92c9205c5278b0
Dings.Rowan R <rowan.dings@student.fontys.nl>
2016-05-27 11:07:06 +0200

    Fixed team, references
    Added operators to team so they could be checked for their given
    teamnumber
    added a win condition in the game class, fixed raiseScore

--- Assets/Scripts/IFieldObject.cs
+++ Assets/Scripts/IFieldObject.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Assets.Scripts
+public interface IFieldObject
 {
-    public interface IFieldObject
-    {
-        /// <summary>
-        /// Method for calculating the distance of whether an object is done with its action or not
-        /// </summary>
-        /// <returns>Whether it has done its action</returns>
-        bool IsActionDone();
-    }
+    /// <summary>
+    /// Method for calculating the distance of whether an object is done with its action or not
+    /// </summary>
+    /// <returns>Whether it has done its action</returns>
+    bool IsActionDone();
 }
+
