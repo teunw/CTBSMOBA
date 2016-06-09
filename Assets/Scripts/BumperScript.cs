8ea459ece8b5617cbf6150ef4b169168f9c61b3e
Joel <j.bleeker@student.fontys.nl>
2016-06-09 15:45:57 +0200

    Collision fixes

---
+++ Assets/Scripts/BumperScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    public class BumperScript : MonoBehaviour
+    {
+        public void OnCollisionEnter2D(Collision2D other)
+        {
+            Debug.Log(other);
+            Debug.Log(other.collider.name);
+            Debug.Log(other.collider.gameObject.name);
+            other.collider.GetComponent<Member>().WallHit();
+        }
+    }
+}
